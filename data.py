7d7043b180fbb23522284e54c266b390725e3d31
kashee ram <kashee.nitd2016@gmail.com>
2017-08-07 00:36:15 +0530

    First commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet,consecter edipistinf',
+            'author':'Kashee Ram Kushwaha',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet,consecter edipistinf',
+            'author': 'Brad Traversy',
+            'create_date': '04-30-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet,consecter edipistinf',
+            'author': 'Kashee Ram Kushwaha',
+            'create_date': '04-25-2017'
+        }
+    ]
+    return articles
